19dddc36b2da1dfa7aaefa49b85e1193eee0d313
dparaiso <danieva.paraiso@gmail.com>
2024-03-21 11:43:37 -0700

    adding hm13.ino

---
+++ build/app/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/danieva/cmpt433/work/project/SyncBeats/app/src/badmath.c" "/home/danieva/cmpt433/work/project/SyncBeats/build/app/CMakeFiles/hello_world.dir/src/badmath.c.o"
+  "/home/danieva/cmpt433/work/project/SyncBeats/app/src/hm13.c" "/home/danieva/cmpt433/work/project/SyncBeats/build/app/CMakeFiles/hello_world.dir/src/hm13.c.o"
+  "/home/danieva/cmpt433/work/project/SyncBeats/app/src/main.c" "/home/danieva/cmpt433/work/project/SyncBeats/build/app/CMakeFiles/hello_world.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../app/include"
+  "../hal/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/danieva/cmpt433/work/project/SyncBeats/build/hal/CMakeFiles/hal.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/app/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.dir/src/badmath.c.o"
+  "CMakeFiles/hello_world.dir/src/hm13.c.o"
+  "CMakeFiles/hello_world.dir/src/main.c.o"
+  "hello_world"
+  "hello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
