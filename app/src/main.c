59fbe146c70d73e973b7d453e3436e5ec4816935
Manav meghpara <73911083+manavmeghpara@users.noreply.github.com>
2024-03-23 12:32:41 -0700

    uart

--- app/src/main.c
+++ app/src/main.c
@@ -1,50 +1,103 @@
-// Main program to build the application
-// Has main(); does initialization and cleanup and perhaps some basic logic.
-
+#include "hal/uart.h"
+#include <pthread.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include "badmath.h"
-#include "hal/button.h"
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_LENGTH 1024
+// file for reading and writing to
+#define UART_DEVICE "/dev/ttyS2"
+
+void* readDataFromBle(void* fd)
+{
+    int uartFileDescriptor = *((int*)fd);
+	printf("Thread 1 Reading...\n");
+    char byte [1024];
+    while(1){
+        uart_read(uartFileDescriptor, &byte);
+    }
+    pthread_exit(NULL);
+}
+
+void* writeToBle(void* fd)
+{
+    int uartFileDescriptor = *((int*)fd);
+	printf("Thread 2 Writing 0xFE\n");
+    uart_write(uartFileDescriptor, 0xFE);
+    pthread_exit(NULL);
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    int fd = uart_init(UART_DEVICE);
 
-    // Initialize all modules; HAL modules first
-    button_init();
-    badmath_init();
+    pthread_t readTid;
+    pthread_t writeTid;
 
-    // Main program logic:
-    for (int i = 0; i < 10; i++) {
-        printf("  -> Reading button time %d = %d\n", i, button_is_button_pressed());
-    }
+    pthread_create(&readTid, NULL, readDataFromBle, (void*)&fd);
+    pthread_create(&writeTid, NULL, writeToBle, (void*)&fd);
+    while(1);
+    pthread_join(readTid, NULL);
+    pthread_join(writeTid, NULL);
 
-    for (int i = 0; i <= 35; i++) {
-        int ans = badmath_factorial(i);
-        printf("%4d! = %6d\n", i, ans);
-    }
-
-    // Cleanup all modules (HAL modules last)
-    badmath_cleanup();
-    button_cleanup();
-
-    printf("!!! DONE !!!\n"); 
-
-    // Some bad code to try out and see what shows up.
-    #if 0
-        // Test your linting setup
-        //   - You should see a warning underline in VS Code
-        //   - You should see compile-time errors when building (-Wall -Werror)
-        // (Linting using clang-tidy; see )
-        int x = 0;
-        if (x = 10) {
-        }
-    #endif
-    #if 0
-        // Demonstrate -fsanitize=address (enabled in the root CMakeFiles.txt)
-        // Compile and run this code. Should see warning at compile time; error at runtime.
-        int data[3];
-        data[3] = 10;
-        printf("Value: %d\n", data[3]);
-    #endif
+    close(fd);
+    return 0;
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <pthread.h>
+// #include <fcntl.h>
+// #include <errno.h>
+
+// #define DEVICE_FILE "/dev/ttyS2"
+
+// void *read_from_serial(void *arg) {
+//     int fd;
+//     char buffer[1024]; // Adjust buffer size as needed
+
+//     // Open the serial port file for reading
+//     fd = open(DEVICE_FILE, O_RDONLY);
+//     if (fd == -1) {
+//         perror("Failed to open serial port");
+//         pthread_exit(NULL);
+//     }
+
+//     // Continuously read from the serial port and print the data
+//     while (1) {
+//         ssize_t bytes_read = read(fd, buffer, sizeof(buffer));
+//         if (bytes_read == -1) {
+//             perror("Error reading from serial port");
+//             close(fd);
+//             pthread_exit(NULL);
+//         } else if (bytes_read > 0) {
+//             // Print the read data to the screen
+//             printf("From Bluetooth : %s", buffer);
+//         }
+//     }
+
+//     // Close the serial port file
+//     close(fd);
+
+//     return NULL;
+// }
+
+// int main() {
+//     pthread_t tid;
+
+//     // Create a thread for reading from the serial port
+//     if (pthread_create(&tid, NULL, read_from_serial, NULL) != 0) {
+//         perror("Failed to create thread");
+//         return EXIT_FAILURE;
+//     }
+
+//     // Main thread continues execution
+//     // You can add additional code here if needed
+//     while(1);
+//     // Wait for the thread to terminate (this won't happen in this example)
+//     pthread_join(tid, NULL);
+
+//     return EXIT_SUCCESS;
+// }
---
+++ hal/src/uart.c
@@ -0,0 +1,79 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "hal/uart.h"
+
+int uart_init(const char* UART_DEVICE)
+{
+    int uartFileDescriptor = open(UART_DEVICE, O_RDWR);
+    if (uartFileDescriptor < 0) {
+        perror("Unable to open UART device");
+        return -1;
+    }
+
+    struct termios tty;
+
+    // Set serial port parameters
+    memset(&tty, 0, sizeof(tty));
+    if (tcgetattr(uartFileDescriptor, &tty) != 0) {
+        perror("Error getting serial port attributes");
+        close(uartFileDescriptor);
+        return -1;
+    }
+
+    cfsetospeed(&tty, BAUD_RATE);
+    cfsetispeed(&tty, BAUD_RATE);
+    tty.c_cflag |= (CLOCAL | CREAD); // Enable receiver and set local mode
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8; // 8 data bits
+    tty.c_cflag &= ~PARENB; // No parity
+    tty.c_cflag &= ~CSTOPB; // 1 stop bit
+    tty.c_cflag &= ~CRTSCTS; // Disable hardware flow control
+    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // Raw input
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Disable software flow control
+    tty.c_oflag &= ~OPOST; // Raw output
+    tty.c_cc[VMIN] = 1; // Read at least 1 character tty.c_cc[VTIME] = 5; // Timeout in 0.5 seconds
+
+    if (tcsetattr(uartFileDescriptor, TCSANOW, &tty) != 0) {
+        perror("Error setting serial port attributes");
+        close(uartFileDescriptor);
+        return -1;
+    }
+
+    return uartFileDescriptor;
+}
+
+void uart_command(int uartFileDescriptor, const char* COMMAND, char* response, size_t maxSize)
+{
+    if (write(uartFileDescriptor, COMMAND, strlen(COMMAND)) < 0) {
+        perror("Error executing UART command");
+        return;
+    }
+
+    int numBytesRead = read(uartFileDescriptor, response, maxSize);
+    if (numBytesRead < 0) {
+        perror("Error reading from UART port");
+        return;
+    }
+
+    response[numBytesRead] = '\0';
+}
+
+void uart_read(int uartFileDescriptor, char* rx_buffer)
+{
+    int bytesRead = read(uartFileDescriptor, rx_buffer, 1024);
+    printf("%s\n", rx_buffer);
+    if (bytesRead < 0) {
+        perror("Error occurred while reading UART device file");
+    }
+}
+
+void uart_write(int uartFileDescriptor, uint8_t value)
+{
+    int bytesWritten = write(uartFileDescriptor, &value, 1);
+    if (bytesWritten < 0) {
+        perror("An error occurred while writing to the UART device file");
+    }
+}
